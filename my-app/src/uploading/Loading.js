a0898ebf1b464ff39efe29fd202111d49eaa17f5
pnoffke64 <113635307+pnoffke64@users.noreply.github.com>
2025-03-31 22:02:34 -0400

    poll

--- my-app/src/uploading/Loading.js
+++ my-app/src/uploading/Loading.js
@@ -1,11 +1,35 @@
 import './upload.css';
 import './loader.css';
+
+DB_NAME = 'CatImageLabels'
+
+
 function Loading(props) {
+    imageName = "catImage.jpg";
+
+    const BASE_URL = process.env.REACT_APP_BASE_API_URL;
+    url = BASE_URL + "testing/getResults?file_name=" + imageName
+    
+    function poll() {
+        fetch(url, {
+            method: 'GET',
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+            },
+          }).then(response => response.json())
+          .then(data => {
+            console.log(data);
+            // subreddit;
+            // accuracy;
+          })
+    }
+
     return (
         <div className="info">
             <div className="loader"></div>
             <br />
             <p>Processing your photo...</p>
+            <Button className="button pollButton" onClick={() => poll()}>Poll</Button>
         </div>
         
     )
