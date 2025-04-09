2461b28c6d8a31519a88e4e5915998f00a3b2a23
jl7739 <109109212+snower7@users.noreply.github.com>
2025-04-09 19:12:14 -0400

    resolve api gateway conflict

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -102,49 +102,6 @@ resource "aws_api_gateway_resource" "uploadImage" {
   path_part   = "uploadImage"
 }
 
-resource "aws_api_gateway_method" "UploadImageToS3Options" {
-  rest_api_id   = aws_api_gateway_rest_api.cassidyApi.id
-  resource_id   = aws_api_gateway_resource.uploadImage.id
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method_response" "options_200" {
-    rest_api_id   = aws_api_gateway_rest_api.cassidyApi.id
-    resource_id   = aws_api_gateway_resource.uploadImage.id
-    http_method   = aws_api_gateway_method.UploadImageToS3Options.http_method
-    status_code   = "200"
-    response_models = {
-        "application/json" = "Empty"
-    }
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Headers" = true,
-        "method.response.header.Access-Control-Allow-Methods" = true,
-        "method.response.header.Access-Control-Allow-Origin" = true
-    }
-    depends_on = ["aws_api_gateway_method.UploadImageToS3Options"]
-}
-resource "aws_api_gateway_integration" "options_integration" {
-    rest_api_id   = aws_api_gateway_rest_api.cassidyApi.id
-    resource_id   = aws_api_gateway_resource.uploadImage.id
-    http_method   = aws_api_gateway_method.UploadImageToS3Options.http_method
-    type          = "MOCK"
-    depends_on = ["aws_api_gateway_method.UploadImageToS3Options"]
-}
-
-resource "aws_api_gateway_integration_response" "options_integration_response" {
-    rest_api_id   = aws_api_gateway_rest_api.cassidyApi.id
-    resource_id   = aws_api_gateway_resource.uploadImage.id
-    http_method   = aws_api_gateway_method.UploadImageToS3Options.http_method
-    status_code   = aws_api_gateway_method_response.options_200.status_code
-    response_parameters = {
-        "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Access-Control-Allow-Origin'",
-        "method.response.header.Access-Control-Allow-Methods" = "'GET,OPTIONS,POST,PUT'",
-        "method.response.header.Access-Control-Allow-Origin" = "'*'"
-    }
-    depends_on = ["aws_api_gateway_method.UploadImageToS3Options"]
-}
-
 resource "aws_api_gateway_method" "UploadImageToS3" {
   rest_api_id   = aws_api_gateway_rest_api.cassidyApi.id
   resource_id   = aws_api_gateway_resource.uploadImage.id
